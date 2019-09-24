a8f48583498ef2c3cb0f2a17dee10df7a86d69f4
wb00001086 <abcd1234>
2019-09-24 21:49:39 +0800

    blog :创建项目

---
+++ src/main/java/com/yidiandian/controller/IndexController.java
@@ -0,0 +1,20 @@
+package com.yidiandian.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: 凤凰[小哥哥]
+ * @Date: 2019/9/24 21:46
+ * @Email: 15290810931@163.com
+ */
+@Slf4j
+@RestController
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
