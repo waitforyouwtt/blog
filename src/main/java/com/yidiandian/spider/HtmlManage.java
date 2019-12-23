73dae68e3835570ab530665308acd7981c9ad09c
wb00001086 <abcd1234>
2019-12-23 10:30:30 +0800

    1. :sparkles: 爬虫音乐

---
+++ src/main/java/com/yidiandian/spider/HtmlManage.java
@@ -0,0 +1,60 @@
+package com.yidiandian.spider;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author: 凤凰[小哥哥]
+ * @Date: 2019/12/23 10:10
+ * @Email: 15290810931@163.com
+ */
+public class HtmlManage {
+
+    public Document manage(String html){
+        Document doc = Jsoup.parse(html);
+        return doc;
+    }
+
+    public Document manageDirect(String url) throws IOException {
+        Document doc = Jsoup.connect( url ).get();
+        return doc;
+    }
+
+    public List<String> manageHtmlTag(Document doc, String tag ){
+        List<String> list = new ArrayList<String>();
+
+        Elements elements = doc.getElementsByTag(tag);
+        for(int i = 0; i < elements.size() ; i++){
+            String str = elements.get(i).html();
+            list.add(str);
+        }
+        return list;
+    }
+
+    public List<String> manageHtmlClass(Document doc,String clas ){
+        List<String> list = new ArrayList<String>();
+
+        Elements elements = doc.getElementsByClass(clas);
+        for(int i = 0; i < elements.size() ; i++){
+            String str = elements.get(i).html();
+            list.add(str);
+        }
+        return list;
+    }
+
+    public List<String> manageHtmlKey(Document doc,String key,String value ){
+        List<String> list = new ArrayList<String>();
+
+        Elements elements = doc.getElementsByAttributeValue(key, value);
+        for(int i = 0; i < elements.size() ; i++){
+            String str = elements.get(i).html();
+            list.add(str);
+        }
+        return list;
+    }
+}
